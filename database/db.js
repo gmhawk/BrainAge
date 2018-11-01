1dac5e84ee2b2ab5a22f9820b6c88b01c64c10c6
gmhawk <alanfu@live.com>
2018-11-01 12:17:30 -0700

    paused db

--- database/db.js
+++ database/db.js
@@ -1,27 +1,27 @@
-const { Client } = require('pg');
-
-const client = new Client({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'brain',
-  password: 'alanfu1337',
-  port: 5432,
-});
-
-client.connect((err, results) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('Database connected');
-  }
-});
-
-const addUser = (user, callback) => {
-  client.query(`INSERT INTO users(id, user) VALUES(${id}, ${user})`, callback);
-};
-
-const getUsers = (callback) => {
-  client.query('SELECT * FROM users', callback);
-};
-
-module.exports = { addUser, getUsers };
+// const { Client } = require('pg');
+//
+// const client = new Client({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'brain',
+//   password: 'alanfu1337',
+//   port: 5432,
+// });
+//
+// client.connect((err, results) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log('Database connected');
+//   }
+// });
+//
+// const addUser = (user, callback) => {
+//   client.query(`INSERT INTO users(id, user) VALUES(${id}, ${user})`, callback);
+// };
+//
+// const getUsers = (callback) => {
+//   client.query('SELECT * FROM users', callback);
+// };
+//
+// module.exports = { addUser, getUsers };
